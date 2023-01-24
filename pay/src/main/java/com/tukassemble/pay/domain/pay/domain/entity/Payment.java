acef4f7cedc9911b6e3e5292154716cfe744e7a1
JeongHunHui <deca3248@gmail.com>
2023-01-24 19:33:25 +0900

    feat: Payment 엔티티 추가

---
+++ pay/src/main/java/com/tukassemble/pay/domain/pay/domain/entity/Payment.java
@@ -0,0 +1,38 @@
+package com.tukassemble.pay.domain.pay.domain.entity;
+
+import com.tukassemble.pay.global.domain.BaseTimeEntity;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "payment")
+public class Payment extends BaseTimeEntity {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(name = "user_id", nullable = false)
+  private Long userId;
+
+  @Column(name = "rental_id", nullable = false)
+  private Long rentalId;
+
+  @Column(name = "pay_amount", nullable = false)
+  private Long payAmount;
+
+  @Enumerated(EnumType.STRING)
+  @Column(name = "payment_status", nullable = false)
+  private PaymentStatus paymentStatus;
+
+  @Builder
+  public Payment(Long userId, Long rentalId, Long payAmount, PaymentStatus paymentStatus) {
+    this.userId = userId;
+    this.rentalId = rentalId;
+    this.payAmount = payAmount;
+    this.paymentStatus = paymentStatus;
+  }
+}
