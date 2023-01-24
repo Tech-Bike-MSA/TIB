02fd50e949637c50101b4ecaac9cfb8513d22c1a
JeongHunHui <deca3248@gmail.com>
2023-01-24 23:17:41 +0900

    feat: Pay Api 구현 완료(결제 모듈 연결, Kafka 제외)

---
+++ pay/src/main/java/com/tukassemble/pay/domain/pay/controller/PaymentController.java
@@ -0,0 +1,27 @@
+package com.tukassemble.pay.domain.pay.controller;
+
+import com.tukassemble.pay.domain.pay.dto.PaymentRequest;
+import com.tukassemble.pay.domain.pay.dto.PaymentResult;
+import com.tukassemble.pay.domain.pay.service.PaymentService;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+@RequestMapping("/api/v1/pay")
+public class PaymentController {
+    private final PaymentService paymentService;
+    @PostMapping
+    public ResponseEntity<PaymentResult> pay(@Valid @RequestBody PaymentRequest paymentRequest) {
+        PaymentResult paymentResult = paymentService.pay(paymentRequest);
+        // Bike State Change Message Pub 하는 코드 추가 하기
+        return ResponseEntity.ok(paymentResult);
+    }
+}
