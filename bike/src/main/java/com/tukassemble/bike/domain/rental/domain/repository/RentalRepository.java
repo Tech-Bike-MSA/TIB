d43aa72d9c480d02a74f502055d5fd3a483f0ed9
wooseokchoe <chldntjr0425@gmail.com>
2023-01-24 15:31:55 +0900

    feat: add RentalRepository

---
+++ bike/src/main/java/com/tukassemble/bike/domain/rental/domain/repository/RentalRepository.java
@@ -0,0 +1,7 @@
+package com.tukassemble.bike.domain.rental.domain.repository;
+
+import com.tukassemble.bike.domain.rental.domain.entity.Rental;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RentalRepository extends JpaRepository<Rental, Long> {
+}
