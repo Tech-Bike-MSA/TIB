61fc1ddd87b6beba202d083d2f9323d05f56225b
JeongHunHui <deca3248@gmail.com>
2023-01-24 19:33:12 +0900

    feat: PaymentStatus Enum 추가

---
+++ pay/src/main/java/com/tukassemble/pay/domain/pay/domain/entity/PaymentStatus.java
@@ -0,0 +1,15 @@
+package com.tukassemble.pay.domain.pay.domain.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+public enum PaymentStatus {
+    SUCCESS("결제 성공"),
+    FAILURE("결제 실패"),
+    ;
+
+    private final String description;
+}
