5d41523acf11750d6a6e2d96b1b18e65e42055db
kim1387 <kevink0@naver.com>
2023-01-24 21:26:12 +0900

    sample: Kafka sample

--- bike/src/main/java/com/tukassemble/bike/domain/management/service/BikeManageService.java
+++ bike/src/main/java/com/tukassemble/bike/domain/management/service/BikeManageService.java
@@ -1,5 +1,7 @@
 package com.tukassemble.bike.domain.management.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tukassemble.bike.domain.management.domain.entity.Bike;
 import com.tukassemble.bike.domain.management.domain.entity.Location;
 import com.tukassemble.bike.domain.management.domain.entity.UseStatus;
@@ -8,6 +10,7 @@ import com.tukassemble.bike.domain.management.dto.BikeRegisterRequest;
 import com.tukassemble.bike.domain.management.dto.BikeInfo;
 import com.tukassemble.bike.domain.management.dto.mapper.BikeMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,6 +19,7 @@ public class BikeManageService {
 
   private final BikeRepository bikeRepository;
   private final BikeMapper bikeMapper;
+  private final KafkaTemplate<String, String> kafkaTemplate;
 
   public BikeInfo register(BikeRegisterRequest bikeRegisterRequest) {
 
@@ -26,7 +30,15 @@ public class BikeManageService {
             .location(new Location(0.0, 0.0))
             .build();
     Bike savedBike = bikeRepository.save(newBike);
+    ObjectMapper mapper = new ObjectMapper();
+    String jsonInString = "";
+    try {
+      jsonInString = mapper.writeValueAsString(bikeMapper.mapToBikeInfo(savedBike));
+    } catch(JsonProcessingException ex) {
+      ex.printStackTrace();
+    }
 
+    kafkaTemplate.send("my-topic", jsonInString);
     return bikeMapper.mapToBikeInfo(savedBike);
   }
 }
