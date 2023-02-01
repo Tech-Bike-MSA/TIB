10c9a6924ddebbac55c475840ff8ea7744dd5a49
JeongHunHui <deca3248@gmail.com>
2023-02-01 23:51:47 +0900

    refactor: controller와 service 필요없는 부분 주석처리 및 사용시간 계산로직 수정

--- pay/src/main/java/com/tukassemble/pay/domain/pay/controller/PaymentController.java
+++ pay/src/main/java/com/tukassemble/pay/domain/pay/controller/PaymentController.java
@@ -1,7 +1,6 @@
 package com.tukassemble.pay.domain.pay.controller;
 
 import com.tukassemble.pay.domain.pay.dto.PaymentRequest;
-import com.tukassemble.pay.domain.pay.dto.PaymentResult;
 import com.tukassemble.pay.domain.pay.service.PaymentService;
 import javax.validation.Valid;
 import lombok.AccessLevel;
@@ -18,9 +17,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class PaymentController {
   private final PaymentService paymentService;
 
-  @PostMapping
-  public ResponseEntity<PaymentResult> pay(@Valid @RequestBody PaymentRequest paymentRequest) {
-    PaymentResult paymentResult = paymentService.pay(paymentRequest);
-    return ResponseEntity.ok(paymentResult);
-  }
+//  @PostMapping
+//  public ResponseEntity<PaymentResult> pay(@Valid @RequestBody PaymentRequest paymentRequest) {
+//    PaymentResult paymentResult = paymentService.pay(paymentRequest);
+//    return ResponseEntity.ok(paymentResult);
+//  }
 }
--- pay/src/main/java/com/tukassemble/pay/domain/pay/service/PaymentService.java
+++ pay/src/main/java/com/tukassemble/pay/domain/pay/service/PaymentService.java
@@ -1,14 +1,13 @@
 package com.tukassemble.pay.domain.pay.service;
 
-import com.tukassemble.pay.domain.pay.domain.entity.Payment;
-import com.tukassemble.pay.domain.pay.domain.entity.PaymentStatus;
 import com.tukassemble.pay.domain.pay.domain.repository.PaymentRepository;
-import com.tukassemble.pay.domain.pay.dto.PaymentRequest;
-import com.tukassemble.pay.domain.pay.dto.PaymentResult;
 import com.tukassemble.pay.domain.pay.mapper.PaymentMapper;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
@@ -20,17 +19,17 @@ public class PaymentService {
   private final Long costPerMin = 100L;
   private static final String PAYMENT_RESULT_TOPIC = "paymentResult";
 
-  public PaymentResult pay(PaymentRequest paymentRequest) {
-    Long paymentAmount = getPaymentAmountByUseTime(paymentRequest.getUseTime());
-    Boolean isPaymentSuccess = tryPay(paymentRequest.getUserId(), paymentAmount);
-    PaymentStatus paymentStatus = getPaymentStatus(isPaymentSuccess);
-    Payment payment = paymentMapper.toEntity(paymentRequest, paymentAmount, paymentStatus);
-    paymentRepository.save(payment);
-
-    // 임시로 일단 결제 성공, 실패 여부만 전송
-    kafkaProducerService.sendMessage(PAYMENT_RESULT_TOPIC, paymentStatus.getDescription());
-    return paymentMapper.toDto(payment);
-  }
+//  public PaymentResult pay(PaymentRequest paymentRequest) {
+//    Long paymentAmount = getPaymentAmountByUseTime(paymentRequest.getRentaledAt(), paymentRequest.getReturnedAt());
+//    Boolean isPaymentSuccess = tryPay(paymentRequest.getUserId(), paymentAmount);
+//    PaymentStatus paymentStatus = getPaymentStatus(isPaymentSuccess);
+//    Payment payment = paymentMapper.toEntity(paymentRequest, paymentAmount, paymentStatus);
+//    paymentRepository.save(payment);
+//
+//    // 임시로 일단 결제 성공, 실패 여부만 전송
+//    kafkaProducerService.sendMessage(PAYMENT_RESULT_TOPIC, paymentStatus.getDescription());
+//    return paymentMapper.toDto(payment);
+//  }
 
   private Boolean tryPay(Long userId, Long paymentAmount) {
     // 실제 결제 모듈이 있다면 여기서 결제, 결제 성공 시 true, 실패 시 false
@@ -38,12 +37,8 @@ public class PaymentService {
     return isPaymentSuccess;
   }
 
-  private PaymentStatus getPaymentStatus(Boolean isPaymentSuccess) {
-    if (isPaymentSuccess) return PaymentStatus.SUCCESS;
-    return PaymentStatus.FAILURE;
-  }
-
-  private Long getPaymentAmountByUseTime(Long useTime) {
-    return useTime * costPerMin;
+  private Long getPaymentAmountByUseTime(LocalDateTime rentaledAt, LocalDateTime returnedAt) {
+    Duration diff = Duration.between(rentaledAt, returnedAt);
+    return diff.toMinutes() * costPerMin;
   }
 }
