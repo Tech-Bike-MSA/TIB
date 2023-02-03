6b47b3fa28cad620390f34616e655d468dfc7326
JeongHunHui <deca3248@gmail.com>
2023-02-04 01:41:28 +0900

    feat: kafka consumer 설정 추가

---
+++ pay/src/main/java/com/tukassemble/pay/global/config/KafkaConfigConsumer.java
@@ -0,0 +1,42 @@
+package com.tukassemble.pay.global.config;
+
+import com.tukassemble.pay.domain.pay.dto.PaymentMessage;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.*;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaConfigConsumer {
+  @Value("${spring.kafka.consumer.bootstrap-servers}")
+  private String consumerBootstrapServers;
+
+  @Value("${spring.kafka.consumer.group-id}")
+  private String consumerGroupId;
+
+  @Bean
+  public ConsumerFactory<String, PaymentMessage> consumerFactory() {
+    Map<String, Object> properties = new HashMap<>();
+    properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, consumerBootstrapServers);
+    properties.put(ConsumerConfig.GROUP_ID_CONFIG, consumerGroupId);
+    properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+    properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+    return new DefaultKafkaConsumerFactory<>(properties);
+  }
+
+  @Bean
+  public ConcurrentKafkaListenerContainerFactory<String, PaymentMessage>
+      kafkaListenerContainerFactory() {
+    ConcurrentKafkaListenerContainerFactory<String, PaymentMessage> kafkaListenerContainerFactory =
+        new ConcurrentKafkaListenerContainerFactory<>();
+    kafkaListenerContainerFactory.setConsumerFactory(consumerFactory());
+    return kafkaListenerContainerFactory;
+  }
+}
