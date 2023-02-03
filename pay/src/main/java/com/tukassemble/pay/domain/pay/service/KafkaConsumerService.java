387da1c906cc3bce33e3fc06b894b9904730fa00
JeongHunHui <deca3248@gmail.com>
2023-02-04 01:45:39 +0900

    feat: 자전거 반납 message를 받아서 결제 및 포인트 적립을 처리하는 Consumer Service 구현

---
+++ pay/src/main/java/com/tukassemble/pay/domain/pay/service/KafkaConsumerService.java
@@ -0,0 +1,27 @@
+package com.tukassemble.pay.domain.pay.service;
+
+import com.tukassemble.pay.domain.pay.domain.entity.Payment;
+import com.tukassemble.pay.domain.pay.dto.PaymentMessage;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class KafkaConsumerService {
+    private final PaymentService paymentService;
+
+    @KafkaListener(
+            groupId = "${spring.kafka.consumer.group-id}",
+            topics = "${spring.kafka.topic.name}"
+    )
+    public void pay(PaymentMessage paymentMessage) {
+        Payment paymentResult = paymentService.pay(paymentMessage);
+        if(paymentResult.getIsPaid()) {
+            // point 적립 message pub
+            // kafkaProducerService.sendMessage(POINT_EARN_TOPIC, pointMessage);
+        }
+    }
+}
