473951bd3e4ca2ba9da7f0c2e92a5b00ee886a45
JeongHunHui <deca3248@gmail.com>
2023-02-04 15:05:30 +0900

    feat: pay의 기능을 사용해보기 위해 임시로 pay api 추가

--- pay/src/main/java/com/tukassemble/pay/domain/pay/controller/PaymentController.java
+++ pay/src/main/java/com/tukassemble/pay/domain/pay/controller/PaymentController.java
@@ -1,10 +1,17 @@
 package com.tukassemble.pay.domain.pay.controller;
 
+import com.tukassemble.pay.domain.pay.domain.entity.Payment;
+import com.tukassemble.pay.domain.pay.dto.PaymentMessage;
 import com.tukassemble.pay.domain.pay.service.PaymentService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
@@ -12,9 +19,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class PaymentController {
   private final PaymentService paymentService;
 
-  //  @PostMapping
-  //  public ResponseEntity<PaymentResult> pay(@Valid @RequestBody PaymentRequest paymentRequest) {
-  //    PaymentResult paymentResult = paymentService.pay(paymentRequest);
-  //    return ResponseEntity.ok(paymentResult);
-  //  }
+  // pay 의 기능을 임시로 사용 해보기 위한 API
+  @PostMapping
+  public ResponseEntity<Payment> pay(@Valid @RequestBody PaymentMessage paymentMessage) {
+    Payment paymentResult = paymentService.pay(paymentMessage);
+    return ResponseEntity.ok(paymentResult);
+  }
 }
